4c31d40c2bf942dae0e6b499f2c584ad3bf07b56
Bram Borggreve <beeman@beeman.nl>
2024-04-21 14:14:52 +0400

    feat: update to Anchor v0.30.0

--- anchor/src/app-1713693601-exports.ts
+++ anchor/src/app-1713693601-exports.ts
@@ -1,13 +1,15 @@
 // Here we export some useful types and functions for interacting with the Anchor program.
-import { Cluster, PublicKey } from '@solana/web3.js'
-import type { App1713693601 } from '../target/types/app_1713693601'
-import { IDL as App1713693601IDL } from '../target/types/app_1713693601'
+import { Cluster, PublicKey } from '@solana/web3.js';
+import type { App1713693601 } from '../target/types/app_1713693601';
+import { App1713693601 as App1713693601IDL } from '../target/types/app_1713693601';
 
 // Re-export the generated IDL and type
-export { App1713693601, App1713693601IDL }
+export { App1713693601, App1713693601IDL };
 
 // After updating your program ID (e.g. after running `anchor keys sync`) update the value below.
-export const APP_1713693601_PROGRAM_ID = new PublicKey('5SkVTeXdssuUaVxY2EXuJVnhCtWcgrP1AdF4uTPsBh6k')
+export const APP_1713693601_PROGRAM_ID = new PublicKey(
+  '5SkVTeXdssuUaVxY2EXuJVnhCtWcgrP1AdF4uTPsBh6k'
+);
 
 // This is a helper function to get the program ID for the App1713693601 program depending on the cluster.
 export function getApp1713693601ProgramId(cluster: Cluster) {
@@ -16,6 +18,6 @@ export function getApp1713693601ProgramId(cluster: Cluster) {
     case 'testnet':
     case 'mainnet-beta':
     default:
-      return APP_1713693601_PROGRAM_ID
+      return APP_1713693601_PROGRAM_ID;
   }
 }
--- anchor/target/types/app_1713693601.ts
+++ anchor/target/types/app_1713693601.ts
@@ -1,191 +1,166 @@
+/**
+ * Program IDL in camelCase format in order to be used in JS/TS.
+ *
+ * Note that this is only a type helper and is not the actual IDL. The original
+ * IDL can be found at `target/idl/app_1713693601.json`.
+ */
 export type App1713693601 = {
-  version: '0.1.0'
-  name: 'app_1713693601'
-  instructions: [
+  "address": "5SkVTeXdssuUaVxY2EXuJVnhCtWcgrP1AdF4uTPsBh6k",
+  "metadata": {
+    "name": "app1713693601",
+    "version": "0.1.0",
+    "spec": "0.1.0",
+    "description": "Created with Anchor"
+  },
+  "instructions": [
     {
-      name: 'close'
-      accounts: [
+      "name": "close",
+      "discriminator": [
+        98,
+        165,
+        201,
+        177,
+        108,
+        65,
+        206,
+        96
+      ],
+      "accounts": [
         {
-          name: 'payer'
-          isMut: true
-          isSigner: true
+          "name": "payer",
+          "writable": true,
+          "signer": true
         },
         {
-          name: 'app1713693601'
-          isMut: true
-          isSigner: false
-        },
-      ]
-      args: []
+          "name": "app1713693601",
+          "writable": true
+        }
+      ],
+      "args": []
     },
     {
-      name: 'decrement'
-      accounts: [
+      "name": "decrement",
+      "discriminator": [
+        106,
+        227,
+        168,
+        59,
+        248,
+        27,
+        150,
+        101
+      ],
+      "accounts": [
         {
-          name: 'app1713693601'
-          isMut: true
-          isSigner: false
-        },
-      ]
-      args: []
+          "name": "app1713693601",
+          "writable": true
+        }
+      ],
+      "args": []
     },
     {
-      name: 'increment'
-      accounts: [
+      "name": "increment",
+      "discriminator": [
+        11,
+        18,
+        104,
+        9,
+        104,
+        174,
+        59,
+        33
+      ],
+      "accounts": [
         {
-          name: 'app1713693601'
-          isMut: true
-          isSigner: false
-        },
-      ]
-      args: []
+          "name": "app1713693601",
+          "writable": true
+        }
+      ],
+      "args": []
     },
     {
-      name: 'initialize'
-      accounts: [
+      "name": "initialize",
+      "discriminator": [
+        175,
+        175,
+        109,
+        31,
+        13,
+        152,
+        155,
+        237
+      ],
+      "accounts": [
         {
-          name: 'payer'
-          isMut: true
-          isSigner: true
+          "name": "payer",
+          "writable": true,
+          "signer": true
         },
         {
-          name: 'app1713693601'
-          isMut: true
-          isSigner: true
+          "name": "app1713693601",
+          "writable": true,
+          "signer": true
         },
         {
-          name: 'systemProgram'
-          isMut: false
-          isSigner: false
-        },
-      ]
-      args: []
+          "name": "systemProgram",
+          "address": "11111111111111111111111111111111"
+        }
+      ],
+      "args": []
     },
     {
-      name: 'set'
-      accounts: [
+      "name": "set",
+      "discriminator": [
+        198,
+        51,
+        53,
+        241,
+        116,
+        29,
+        126,
+        194
+      ],
+      "accounts": [
         {
-          name: 'app1713693601'
-          isMut: true
-          isSigner: false
-        },
-      ]
-      args: [
+          "name": "app1713693601",
+          "writable": true
+        }
+      ],
+      "args": [
         {
-          name: 'value'
-          type: 'u8'
-        },
+          "name": "value",
+          "type": "u8"
+        }
       ]
-    },
-  ]
-  accounts: [
+    }
+  ],
+  "accounts": [
     {
-      name: 'app1713693601'
-      type: {
-        kind: 'struct'
-        fields: [
+      "name": "app1713693601",
+      "discriminator": [
+        7,
+        240,
+        137,
+        140,
+        196,
+        236,
+        68,
+        97
+      ]
+    }
+  ],
+  "types": [
+    {
+      "name": "app1713693601",
+      "type": {
+        "kind": "struct",
+        "fields": [
           {
-            name: 'count'
-            type: 'u8'
-          },
+            "name": "count",
+            "type": "u8"
+          }
         ]
       }
-    },
+    }
   ]
-}
-
-export const IDL: App1713693601 = {
-  version: '0.1.0',
-  name: 'app_1713693601',
-  instructions: [
-    {
-      name: 'close',
-      accounts: [
-        {
-          name: 'payer',
-          isMut: true,
-          isSigner: true,
-        },
-        {
-          name: 'app1713693601',
-          isMut: true,
-          isSigner: false,
-        },
-      ],
-      args: [],
-    },
-    {
-      name: 'decrement',
-      accounts: [
-        {
-          name: 'app1713693601',
-          isMut: true,
-          isSigner: false,
-        },
-      ],
-      args: [],
-    },
-    {
-      name: 'increment',
-      accounts: [
-        {
-          name: 'app1713693601',
-          isMut: true,
-          isSigner: false,
-        },
-      ],
-      args: [],
-    },
-    {
-      name: 'initialize',
-      accounts: [
-        {
-          name: 'payer',
-          isMut: true,
-          isSigner: true,
-        },
-        {
-          name: 'app1713693601',
-          isMut: true,
-          isSigner: true,
-        },
-        {
-          name: 'systemProgram',
-          isMut: false,
-          isSigner: false,
-        },
-      ],
-      args: [],
-    },
-    {
-      name: 'set',
-      accounts: [
-        {
-          name: 'app1713693601',
-          isMut: true,
-          isSigner: false,
-        },
-      ],
-      args: [
-        {
-          name: 'value',
-          type: 'u8',
-        },
-      ],
-    },
-  ],
-  accounts: [
-    {
-      name: 'app1713693601',
-      type: {
-        kind: 'struct',
-        fields: [
-          {
-            name: 'count',
-            type: 'u8',
-          },
-        ],
-      },
-    },
-  ],
-}
+};
